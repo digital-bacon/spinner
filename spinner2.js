c57882c38d015487e6f6cc9c9206d52862fd5fe3
Mark Bieganek <mark.bieganek@telus.com>
2022-11-22 17:32:49 -0700

    Add comments for clarity, add newline after processing

--- spinner2.js
+++ spinner2.js
@@ -17,14 +17,19 @@ const spinner = (spinnerStartDelay = 0, spinnerFrameDuration = 100) => {
     frame4: '\\' // Escape the backslash since it's a special character 
   };
   let currentDelay = spinnerStartDelay;
+  // spinnerFrames define 1/2 rotation, so show them twice for a full rotation
   for (let repeatSpinnerFrames = 2; repeatSpinnerFrames > 0; repeatSpinnerFrames--) {
     for (const frame of spinnerFrames.order) {
+      // Define what will be printed to the terminal in this frame
       let currentFrameOutput = stdOutReset + spinnerFrames[frame] + padding;
+      // Print the current frame to the terminal
       setTimeout(() => {process.stdout.write(currentFrameOutput)}, currentDelay);
       // Set the delay to use for the next frame in sequence
       currentDelay += spinnerFrameDuration;
     }
   }
+  // Move the terminal prompt to the next line when done
+  setTimeout(() => {process.stdout.write('\n')}, currentDelay + spinnerFrameDuration);
 };
 
 // Test case
